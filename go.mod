42b33e5c847dd642dd1a0e14d0608621f86c4ae1
plantimals <5475390+plantimals@users.noreply.github.com>
2021-03-07 23:13:14 -0600

    add json tags to export opml as json

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/plantimals/go-opml
+
+go 1.16
+
+require github.com/gilliek/go-opml v1.0.0
--- opml/opml.go
+++ opml/opml.go
@@ -22,50 +22,50 @@ import (
 // OPML is the root node of an OPML document. It only has a single required
 // attribute: the version.
 type OPML struct {
-	XMLName xml.Name `xml:"opml"`
-	Version string   `xml:"version,attr"`
-	Head    Head     `xml:"head"`
-	Body    Body     `xml:"body"`
+	XMLName xml.Name `xml:"opml" json:"opml"`
+	Version string   `xml:"version,attr" json:"version"`
+	Head    Head     `xml:"head" json:"head"`
+	Body    Body     `xml:"body" json:"body"`
 }
 
 // Head holds some meta information about the document.
 type Head struct {
-	Title           string `xml:"title"`
-	DateCreated     string `xml:"dateCreated,omitempty"`
-	DateModified    string `xml:"dateModified,omitempty"`
-	OwnerName       string `xml:"ownerName,omitempty"`
-	OwnerEmail      string `xml:"ownerEmail,omitempty"`
-	OwnerID         string `xml:"ownerId,omitempty"`
-	Docs            string `xml:"docs,omitempty"`
-	ExpansionState  string `xml:"expansionState,omitempty"`
-	VertScrollState string `xml:"vertScrollState,omitempty"`
-	WindowTop       string `xml:"windowTop,omitempty"`
-	WindowBottom    string `xml:"windowBottom,omitempty"`
-	WindowLeft      string `xml:"windowLeft,omitempty"`
-	WindowRight     string `xml:"windowRight,omitempty"`
+	Title           string `xml:"title" json:"title"`
+	DateCreated     string `xml:"dateCreated,omitempty" json:"dateCreated,omitempty"`
+	DateModified    string `xml:"dateModified,omitempty" json:"dateModified,omitempty"`
+	OwnerName       string `xml:"ownerName,omitempty" json:"ownerName,omitempty"`
+	OwnerEmail      string `xml:"ownerEmail,omitempty" json:"ownerEmail,omitempty"`
+	OwnerID         string `xml:"ownerId,omitempty" json:"ownerId,omitempty"`
+	Docs            string `xml:"docs,omitempty" json:"docs,omitempty"`
+	ExpansionState  string `xml:"expansionState,omitempty" json:"expansionState,omitempty"`
+	VertScrollState string `xml:"vertScrollState,omitempty json:"verScrollState,omitempty"`
+	WindowTop       string `xml:"windowTop,omitempty" json:"windowTop,omitempty"`
+	WindowBottom    string `xml:"windowBottom,omitempty" json:"windowBottom,omitempty"`
+	WindowLeft      string `xml:"windowLeft,omitempty" json:"windowLeft,omitempty"`
+	WindowRight     string `xml:"windowRight,omitempty" json:"windowRight,omitempty"`
 }
 
 // Body is the parent structure of all outlines.
 type Body struct {
-	Outlines []Outline `xml:"outline"`
+	Outlines []Outline `xml:"outline" json:"outline"`
 }
 
 // Outline holds all information about an outline.
 type Outline struct {
-	Outlines     []Outline `xml:"outline"`
-	Text         string    `xml:"text,attr"`
-	Type         string    `xml:"type,attr,omitempty"`
-	IsComment    string    `xml:"isComment,attr,omitempty"`
-	IsBreakpoint string    `xml:"isBreakpoint,attr,omitempty"`
-	Created      string    `xml:"created,attr,omitempty"`
-	Category     string    `xml:"category,attr,omitempty"`
-	XMLURL       string    `xml:"xmlUrl,attr,omitempty"`
-	HTMLURL      string    `xml:"htmlUrl,attr,omitempty"`
-	URL          string    `xml:"url,attr,omitempty"`
-	Language     string    `xml:"language,attr,omitempty"`
-	Title        string    `xml:"title,attr,omitempty"`
-	Version      string    `xml:"version,attr,omitempty"`
-	Description  string    `xml:"description,attr,omitempty"`
+	Outlines     []Outline `xml:"outline" json:"outline"`
+	Text         string    `xml:"text,attr" json:"text"`
+	Type         string    `xml:"type,attr,omitempty" json:"type,omitempty"`
+	IsComment    string    `xml:"isComment,attr,omitempty" json:"isComment,omitempty"`
+	IsBreakpoint string    `xml:"isBreakpoint,attr,omitempty" json:"isBreakpoint,omitempty"`
+	Created      string    `xml:"created,attr,omitempty" json:"created,omitempty"`
+	Category     string    `xml:"category,attr,omitempty" json:"category,omitempty"`
+	XMLURL       string    `xml:"xmlUrl,attr,omitempty" json:"xmlUrl,omitempty"`
+	HTMLURL      string    `xml:"htmlUrl,attr,omitempty" json:"htmlUrl,omitempty"`
+	URL          string    `xml:"url,attr,omitempty" json:"url,omitempty"`
+	Language     string    `xml:"language,attr,omitempty" json:"language,omitempty"`
+	Title        string    `xml:"title,attr,omitempty" json:"title,omitempty"`
+	Version      string    `xml:"version,attr,omitempty" json:"version,omitempty"`
+	Description  string    `xml:"description,attr,omitempty" json:"description,omitempty"`
 }
 
 // NewOPML creates a new OPML structure from a slice of bytes.
